2a1f923d8af60c12ffb9aa1a30a7bc72d1758a45
RyanMorris95 <morrisryanmorris@gmail.com>
2018-01-24 17:10:06 -0800

    winograd and gpu fraction

--- train.py
+++ train.py
@@ -1,28 +1,24 @@
 import functools
 
 import tensorflow as tf
-
+import os
 import architectures
 import memory_saving_gradients
-
-# monkey patch tf.gradients to point to our custom version, with automatic checkpoint selection
-tf.__dict__["gradients"] = memory_saving_gradients.gradients_memory
 
 from tensorflow.contrib.learn import ModeKeys
 from tensorflow.contrib.learn import learn_runner
 from kings_dataset import KingsDataset
 
-keras = tf.keras
-
-optimizer = tf.train.AdagradOptimizer
+os.environ["TF_ENABLE_WINOGRAD_NONFUSED"] = '1'
 
 flags = tf.app.flags
 flags.DEFINE_string("dataset_dir", "/media/ryan/E4DE46CCDE4696A8/KingsCollege/", "Dataset directory")
 flags.DEFINE_string("optimizer", "adam", "Optimizer type")
 flags.DEFINE_string("loss", "l1", "type of norm loss")
+flags.DEFINE_string("save_dir", None, "Directory to save checkpoints")
 flags.DEFINE_float("grad_clip", None, "Gradient Norm Clipping")
 flags.DEFINE_float("learning_rate", 0.001, "learing rate of optimizer")
-flags.DEFINE_float("gpu_usage", 1.0, "% usage of gpu")
+flags.DEFINE_float("gpu_fraction", 1.0, "% usage of gpu")
 flags.DEFINE_integer("batch_size", 8, "The size of of a sample batch")
 flags.DEFINE_integer("img_height", 224, "Image height")
 flags.DEFINE_integer("img_width", 224, "Image width")
@@ -30,12 +26,17 @@ flags.DEFINE_integer("train_size", 1219, "Size of training set")
 flags.DEFINE_integer("eval_size", 342, "Size of eval set")
 flags.DEFINE_integer("epochs", 300, "Number of runs through dataset")
 flags.DEFINE_bool("abs_and_rel", True, "Combine absolute and relative pose losses")
+flags.DEFINE_bool("mem_save_grad", True, "Use memory saving gradients.")
 FLAGS = flags.FLAGS
 
 TRAIN_STEPS = int(FLAGS.train_size / FLAGS.batch_size)
 EVAL_STEPS = int(FLAGS.eval_size / FLAGS.batch_size)
-print ("Train steps: ", str(TRAIN_STEPS))
-print ("Eval steps: ", str(EVAL_STEPS))
+print("Train steps: ", str(TRAIN_STEPS))
+print("Eval steps: ", str(EVAL_STEPS))
+
+if FLAGS.mem_save_grad:
+    # monkey patch tf.gradients to point to our custom version, with automatic checkpoint selection
+    tf.__dict__["gradients"] = memory_saving_gradients.gradients_memory
 
 
 def model_fn(features, labels, mode, params):
@@ -44,7 +45,11 @@ def model_fn(features, labels, mode, params):
     is_training = mode == ModeKeys.TRAIN
     global_step = tf.train.get_global_step()
 
-    predictions = architectures.build_resnet34(features, is_training=is_training)
+    num_outputs = 7
+    if FLAGS.abs_and_rel:
+        num_outputs = 14
+
+    predictions = architectures.build_resnet34(features, num_output=num_outputs, is_training=is_training)
 
     if mode == tf.estimator.ModeKeys.PREDICT:
         return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
@@ -59,36 +64,41 @@ def model_fn(features, labels, mode, params):
         sx = tf.Variable(0, dtype=tf.float32, name='optimal_trans_weight')
         sq = tf.Variable(-3, dtype=tf.float32, name='optimal_rot_weight')
 
-        predictions_rel, predictions_abs = tf.split(predictions, 2, axis=1)
-        pred_abs_trans = tf.slice(predictions_abs, [0, 0], [-1, 3])
-        pred_abs_quat = tf.slice(predictions_abs, [0, 3], [-1, 4])
-        pred_rel_trans = tf.slice(predictions_rel, [0, 0], [-1, 3])
-        pred_rel_quat = tf.slice(predictions_rel, [0, 3], [-1, 4])
+        with tf.variable_scope('label_pred_sclicing'):
+            predictions_rel, predictions_abs = tf.split(predictions, 2, axis=1)
+            pred_abs_trans = tf.slice(predictions_abs, [0, 0], [-1, 3])
+            pred_abs_quat = tf.slice(predictions_abs, [0, 3], [-1, 4])
+            pred_rel_trans = tf.slice(predictions_rel, [0, 0], [-1, 3])
+            pred_rel_quat = tf.slice(predictions_rel, [0, 3], [-1, 4])
 
-        labels_rel, labels_abs = tf.split(labels, 2, axis=1)
-        label_abs_trans = tf.slice(labels_abs, [0, 0], [-1, 3])
-        label_abs_quat = tf.slice(labels_abs, [0, 3], [-1, 4])
-        label_rel_trans = tf.slice(labels_rel, [0, 0], [-1, 3])
-        label_rel_quat = tf.slice(labels_rel, [0, 3], [-1, 4])
+            labels_rel, labels_abs = tf.split(labels, 2, axis=1)
+            label_abs_trans = tf.slice(labels_abs, [0, 0], [-1, 3])
+            label_abs_quat = tf.slice(labels_abs, [0, 3], [-1, 4])
+            label_rel_trans = tf.slice(labels_rel, [0, 0], [-1, 3])
+            label_rel_quat = tf.slice(labels_rel, [0, 3], [-1, 4])
 
-        if FLAGS.loss == 'l1':
-            loss_abs_trans = tf.reduce_mean(tf.losses.absolute_difference(label_abs_trans, pred_abs_trans))
-            loss_abs_quat = tf.reduce_mean(tf.losses.absolute_difference(label_abs_quat, pred_abs_quat))
-        elif FLAGS.loss == 'l2':
-            loss_abs_trans = tf.losses.mean_squared_error(label_abs_trans, pred_abs_trans)
-            loss_abs_quat = tf.losses.mean_squared_error(label_abs_quat, pred_abs_quat)
+        with tf.variable_scope('loss_abs'):
+            if FLAGS.loss == 'l1':
+                loss_abs_trans = tf.reduce_mean(tf.losses.absolute_difference(label_abs_trans, pred_abs_trans))
+                loss_abs_quat = tf.reduce_mean(tf.losses.absolute_difference(label_abs_quat, pred_abs_quat))
+            elif FLAGS.loss == 'l2':
+                loss_abs_trans = tf.losses.mean_squared_error(label_abs_trans, pred_abs_trans)
+                loss_abs_quat = tf.losses.mean_squared_error(label_abs_quat, pred_abs_quat)
 
-        mse_abs = tf.add(loss_abs_trans, loss_abs_quat)
+            mse_abs = tf.add(loss_abs_trans, loss_abs_quat)
 
-        trans_loss_abs = tf.add(tf.scalar_mul(loss_abs_trans, tf.exp(-sx)), sx)
-        rot_loss_abs = tf.add(tf.scalar_mul(loss_abs_quat, tf.exp(-sq)), sq)
+            trans_loss_abs = tf.add(tf.scalar_mul(loss_abs_trans, tf.exp(-sx)), sx)
+            rot_loss_abs = tf.add(tf.scalar_mul(loss_abs_quat, tf.exp(-sq)), sq)
 
-        loss = tf.add(trans_loss_abs, rot_loss_abs, name='loss')
+            loss = tf.add(trans_loss_abs, rot_loss_abs, name='loss_abs')
 
         if FLAGS.abs_and_rel:
-            loss_rel_trans = tf.reduce_mean(tf.losses.absolute_difference(label_rel_trans, pred_rel_trans))
-            loss_rel_quat = tf.reduce_mean(tf.losses.absolute_difference(label_rel_quat, pred_rel_quat))
-            loss = tf.add(loss, tf.add(loss_rel_trans, loss_rel_quat))
+            with tf.variable_scope('loss_rel'):
+                loss_rel_trans = tf.reduce_mean(tf.losses.absolute_difference(label_rel_trans, pred_rel_trans))
+                loss_rel_quat = tf.reduce_mean(tf.losses.absolute_difference(label_rel_quat, pred_rel_quat))
+                loss_rel = tf.add(loss_rel_quat, loss_rel_trans)
+            loss = tf.add(loss, loss_rel, name='loss_absrel')
+            tf.summary.scalar('loss_rel', loss_rel)
 
         lr = FLAGS.learning_rate
 
@@ -129,7 +139,7 @@ def model_fn(features, labels, mode, params):
 def input_fn(data_dir, subset, batch_size, use_distortion_for_training=True):
     with tf.device('/cpu:0'):
         use_distortion = subset == 'train' and use_distortion_for_training
-        dataset = KingsDataset(data_dir, subset, use_distortion)
+        dataset = KingsDataset(data_dir, subset, use_distortion, abs_and_rel=FLAGS.abs_and_rel)
         image_batch, label_batch = dataset.make_batch(batch_size)
         return image_batch, label_batch
 
@@ -155,16 +165,25 @@ def get_experiment_fn(data_dir):
 def main(argv):
     """Run the training experiment."""
     # Define model parameters
+    if FLAGS.abs_and_rel:
+        num_classes = 14
+    else:
+        num_classes = 7
+
     params = tf.contrib.training.HParams(
         learning_rate=FLAGS.learning_rate,
-        n_classes=7,
+        n_classes=num_classes,
         train_steps=TRAIN_STEPS * FLAGS.epochs,
         min_eval_frequency=500
     )
 
+    # Session config
+    config = tf.ConfigProto()
+    config.gpu_options.per_process_gpu_memory_fraction = FLAGS.gpu_fraction
+
     # Set the run_config and the directory to save the model and stats
-    run_config = tf.contrib.learn.RunConfig()
-    run_config = run_config.replace(model_dir='./resnet34')
+    run_config = tf.contrib.learn.RunConfig(session_config=config)
+    run_config = run_config.replace(model_dir='./'+FLAGS.save_dir)
 
     learn_runner.run(
         experiment_fn=get_experiment_fn(FLAGS.dataset_dir),  # First-class function
